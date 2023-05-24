b4500a8f6d801ff6ab9aa400fa76664a2b4a55cc
TruongNguyen <truongmoba157@gmail.com>
2023-05-24 15:54:29 +0700

    tpro-fix: url db

--- src/v1/config/database.config.js
+++ src/v1/config/database.config.js
@@ -20,5 +20,7 @@ export async function connect() {
 
 		mongoose.set("strictQuery", false);
 		await mongoose.connect("mongodb://127.0.0.1:27017/thinkpro");
-	} catch (error) { }
+	} catch (error) {
+		console.log(error)
+	}
 }
